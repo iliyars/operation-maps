7275f242bccda962f42bf6a25a0b4ad418cc9e46
iliyars <iliyars@mail.ru>
2026-05-26 23:04:11 +0300

    feat(domain): add DetectedCategory to ProjectComponent

--- src/OperationMaps.Domain/Entities/Projects/ProjectEntities.cs
+++ src/OperationMaps.Domain/Entities/Projects/ProjectEntities.cs
@@ -32,6 +32,7 @@ public class ProjectComponent
 
   public int? ComponentId { get; set; }
   public Component? Component { get; set; }
+  public string? DetectedCategory { get; set; }
 }
 
 public class RegimeGroup
--- src/OperationMaps.Infrastructure/Persistence/Migrations/20260526192954_AddDetectedCategory.Designer.cs
+++ src/OperationMaps.Infrastructure/Persistence/Migrations/20260526192954_AddDetectedCategory.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OperationMaps.Infrastructure.Persistence;
 
@@ -10,9 +11,11 @@ using OperationMaps.Infrastructure.Persistence;
 namespace OperationMaps.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(OperationMapsDbContext))]
-    partial class OperationMapsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260526192954_AddDetectedCategory")]
+    partial class AddDetectedCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.8");
